698510b29db9849174f3263d9088f54964b0f980
asha132414 <ashajanu12345@gmail.com>
2026-06-03 17:11:16 +0530

    Fix Supabase keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://your-project-id.supabase.co"
-const supabaseAnonKey = "sb_publishable_RH3z04113M2QnQEsWrmd0w_mqzdY9j8"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBycXp2aHdrYmVpcXljb2xybGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMjkyNjUsImV4cCI6MjA5NTgwNTI2NX0.BEPO1fXklUTJerKhufLHa-OcSQmj2nc3p36KQKjtvMs"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
