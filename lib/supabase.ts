c17d8872cb1c2d2aeca8c92127362b4e3d9e811a
asha132414 <ashajanu12345@gmail.com>
2026-06-03 15:25:20 +0530

    Fixing Supabase API connection and security

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://prqzvhwkbeiqycolrlck.supabase.co';
-const supabaseAnonKey = 'sb_publishable_RH3z04113M2QnQuI7KAnT4vsh73Isk3K';
+const supabaseUrl = "https://your-project-id.supabase.co"
+const supabaseAnonKey = "sb_publishable_RH3z04113M2QnQEsWrmd0w_mqzdY9j8"
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
