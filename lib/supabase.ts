a617733b64aa10d195b83df0ed3791b9b74c5613
asha132414 <ashajanu12345@gmail.com>
2026-06-03 20:04:52 +0530

    Fixing Supabase API keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://your-project-id.supabase.co"
+const supabaseUrl = "https://prqzvhwkbeiqycolrlck.supabase.co"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBycXp2aHdrYmVpcXljb2xybGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMjkyNjUsImV4cCI6MjA5NTgwNTI2NX0.BEPO1fXklUTJerKhufLHa-OcSQmj2nc3p36KQKjtvMs"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
